abd82e1d082dc38fb131c57ef558e59e3516cf86
Thiago Figueiredo <tsoaresfigueiredo@gmail.com>
2018-05-31 15:16:23 -0400

    DOJO doceria
    Calculadora de doceria

---
+++ dojo-lari-maia/calculadora.js
@@ -0,0 +1,14 @@
+class Calculadora {
+    calcular() {
+        let porcentagemBonus = document.getElementById('porcentagemBonus').value
+        let celofane = document.getElementById('celofane').value
+        let crepom = document.getElementById('crepom').value
+        let lacoChannel = document.getElementById('lacoChannel').value
+        let importado = document.getElementById('importado').value
+        let caixa = document.getElementById('caixa').value
+
+        alert(porcentagemBonus)
+    }
+}
+
+var calculadora = new Calculadora()
