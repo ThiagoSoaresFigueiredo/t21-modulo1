ac7371abb909dfd2d05ac0fd91bf4ba6ede4410f
Thiago Figueiredo <tsoaresfigueiredo@gmail.com>
2018-05-31 10:29:15 -0400

    Clauladora final
    4 operações básicas

---
+++ exercicio2/js/menu.js
@@ -0,0 +1,7 @@
+function abrirMenu() {
+    document.getElementById('menu').classList.add('menu_aberto')
+}
+
+function fecharMenu() {
+    document.getElementById('menu').classList.remove('menu_aberto')
+}
