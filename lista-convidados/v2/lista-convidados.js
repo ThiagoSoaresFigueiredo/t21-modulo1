494bd5ba9d3b96e1baf72cf48cdbbff2f4f22856
Thiago Figueiredo <tsoaresfigueiredo@gmail.com>
2018-06-01 23:40:08 -0400

    Lista Convidados v2

--- lista-convidados/v2/lista-convidados.js
+++ lista-convidados/v2/lista-convidados.js
@@ -2,15 +2,43 @@ class ListaConvidados {
 
     constructor() {
         this.qtd = 0;
+        this.id = 0;
     }
 
     adicionar() {
         let nome = document.getElementById('inputConvidado').value;
         let lista = document.getElementById('lista');
 
-        lista.innerText += nome + "\n";
+        let div = document.createElement('div');
+        div.classList.add('item-lista');
+        div.id = this.id;
 
-        this.qtd++;
+        let span = document.createElement('span');
+        span.innerText = nome;
+
+        let img = document.createElement('img');
+        img.src = "img/delete01.svg";
+        img.title = "Excluir convidado";
+        img.setAttribute("onclick",
+            "listaConvidados.remover('" + div.id + "')");
+
+        div.appendChild(span);
+        div.appendChild(img);
+
+        lista.appendChild(div);
+
+        this.qtd = lista.getElementsByTagName('div').length;
+        this.id++;
+
+        let labelQtde = document.getElementById('qtde');
+        labelQtde.innerText = "(" + this.qtd + ")";
+    }
+
+    remover(id) {
+        let conv = document.getElementById(id);
+        let lista = document.getElementById('lista');
+        lista.removeChild(conv);
+        this.qtd = lista.getElementsByTagName('div').length;
         let labelQtde = document.getElementById('qtde');
         labelQtde.innerText = "(" + this.qtd + ")";
     }
